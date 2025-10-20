26cc7db24aed4fb469425b4e734afa0db891d3cc
JDjump12345 <91620140+JDjump12345@users.noreply.github.com>
2025-10-20 14:20:53 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,23 +1,6 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
-
-addEventListener('fetch', event => {
-  event.respondWith(handleEvent(event));
+addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => new Response("File not found", { status: 404 }))
+  );
 });
-
-async function handleEvent(event) {
-  try {
-    // Serve the requested file or default to index.html
-    return await getAssetFromKV(event, {
-      mapRequestToAsset: req => {
-        const url = new URL(req.url);
-        // if root, serve index.html
-        if (url.pathname === '/') {
-          url.pathname = '/index.html';
-        }
-        return new Request(url.toString(), req);
-      }
-    });
-  } catch (e) {
-    return new Response('Not found', { status: 404 });
-  }
-}
